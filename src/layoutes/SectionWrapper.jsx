1d79641546f2f5aa94bcfc4ca15e7ba9b2cf980b
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-07-22 16:20:54 +0200

    created SectionWrapper to reduce boilderplate code

---
+++ src/layoutes/SectionWrapper.jsx
@@ -0,0 +1,25 @@
+import SectionTitle from "../components/SectionTitle.jsx";
+
+
+const SectionWrapper = ({mini, title, description, children}) => {
+    return (
+        <section className="my-[150px]">
+            <div className="container mx-auto px-[24px]">
+                <div className="flex items-center justify-center flex-col gap-y-[100px]">
+                    <div className="top flex items-center justify-center flex-col">
+                        <SectionTitle>
+                            <h3>{mini}</h3>
+                        </SectionTitle>
+                        <h1 className="font-bold text-[58px] my-3">{title}</h1>
+                        <p className="font-outfit text-[20px] font-medium text-center" dangerouslySetInnerHTML={{__html: description}} />
+                    </div>
+                    <div className="bottom w-full">
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SectionWrapper
